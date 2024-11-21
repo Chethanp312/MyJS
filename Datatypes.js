72d36be0b7535e0b64812cc3911dcaccc6ed8d25
Chethanp312 <57157929+Chethanp312@users.noreply.github.com>
2024-11-21 19:33:44 +0000

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,28 @@
+/* 
+    1. "Use strict"; - This treates all JS code as newer version.
+    2. Code readability should be high
+    3. ECMA script for standards
+    4. Use MDN for documentation.
+    ---------
+    Datatypes
+    ----------
+    number => 2 to power 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined => no value assigned 
+    symbol => unique
+    ----------
+    Object
+    ----------
+*/
+
+let Name = "Chethan";
+let Age = 35;
+let Location;
+let Balance = "";
+let AccountBalance = null;
+
+
+console.table([typeof Name, typeof Age, typeof Location, typeof Balance, typeof AccountBalance]);
